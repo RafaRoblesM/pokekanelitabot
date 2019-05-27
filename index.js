e9f151d9d3a5d8251529b1bc01726c118cd0025f
Rafa <rafarobles@guelcom.net>
2019-05-27 13:15:22 +0200

    try start in herokku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TOKEN = process.env.TELEGRAM_TOKEN || '807546853:AAGbex1UVaHrO9MIvB2t9LHAnWW_iUazYJE';
 const TelegramBot = require('node-telegram-bot-api');
+const request = require('request')
 const options = {
   webHook: {
     port: process.env.PORT
