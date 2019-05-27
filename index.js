347ecb7265e96329ba244b03abd19a5dd0f1c9f1
Rafa <rafarobles@guelcom.net>
2019-05-27 12:17:50 +0200

    put in herokku

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api')
-const bot = new TelegramBot('807546853:AAGbex1UVaHrO9MIvB2t9LHAnWW_iUazYJE',{polling: true})
+
+const TOKEN = process.env.TELEGRAM_TOKEN || '807546853:AAGbex1UVaHrO9MIvB2t9LHAnWW_iUazYJE';
+const bot = new TelegramBot(TOKEN,{polling: true})
 const request = require('request')
 
-bot.onText(/^\start/, function(msg){
+bot.onText(/^\/start/, function(msg){
   console.log(msg)
   var chatId = msg.chat.id;
   var username = msg.form.username;
@@ -14,6 +16,22 @@ bot.on('message', function(msg){
   var Hola = "hola bot";
   var firstnameuser = msg.from.first_name;
   if(msg.text.toString().toLowerCase().includes(Hola)){
-    bot.sendMessage(msg.chat.id, "Hola " + firstnameuser )
+    bot.sendMessage(msg.chat.id, "Hola pokemaniaco " + firstnameuser )
   }
-})
+});
+bot.on('message', function(msg){
+    var Busca = "buscame";
+    var firstnameuser = msg.from.first_name;
+    if(msg.text.toString().toLowerCase().includes(Busca)){
+      bot.sendMessage(msg.chat.id, "Hola pokemaniaco " + firstnameuser )
+    }
+  });
+  const url = process.env.APP_URL || 'https://pokekanelitabot.herokuapp.com';
+    const bot = new TelegramBot(TOKEN, options);
+
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+
+bot.on('message', function onMessage(msg) {
+  bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
+});
