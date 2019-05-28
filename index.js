6fcd2f8c468de5b78f00970d7d103c8ef6f92079
Rafa <rafarobles@guelcom.net>
2019-05-28 12:55:36 +0200

    weather its ok

--- index.js
+++ index.js
@@ -2,40 +2,58 @@ const TelegramBot = require('node-telegram-bot-api')
 
 const TOKEN = '807546853:AAGbex1UVaHrO9MIvB2t9LHAnWW_iUazYJE';
 const bot = new TelegramBot(TOKEN,{polling: true})
-const request = require('request')
-const url ='https://pokekanelitabot.herokuapp.com';
+const weather = require('weather-js');
 
-bot.onText(/^\/start/, function(msg){
-  console.log(msg)
-  var chatId = msg.chat.id;
-  var username = msg.form.username;
 
-  bot.sendMessage(chatId, "Hola, " + username + " soy el pokebot y me llamo PokekanelitaBot")
-});
+bot.onText(/^\!botones/, function(msg){
+  var chatId =  msg.chat.id;
+  var botones = {
+    reply_markup:{
+      inline_keyboard:[
+        [
+          {text: "boton 1", callback_data: 'boton1'}
+        ]
+      ]
+    }};
 
-bot.on('message', function(msg){
-  var Hola = "hola bot";
-  var firstnameuser = msg.from.first_name;
-  if(msg.text.toString().toLowerCase().includes(Hola)){
-    bot.sendMessage(msg.chat.id, "Hola pokemaniaco " + firstnameuser )
-  }
-});
-bot.on('message', function(msg){
-    var Busca = "hola";
-    var firstnameuser = msg.from.first_name;
-    if(msg.text.toString().toLowerCase().includes(Busca)){
-      bot.sendMessage(msg.chat.id, "T revientoh mamonah " )
-    }
-  });
-  bot.on('message', function(msg){
-    var Busca = "mierda";
-    var firstnameuser = msg.from.first_name;
-    if(msg.text.toString().toLowerCase().includes(Busca)){
-      bot.sendMessage(msg.chat.id, "Tuh muertoh nanoh " )
-    }
-  });
+  bot.sendMessage(chatId, "aqui pongp el boton", botones)
   
-    bot.setWebHook(`${url}/bot${TOKEN}`);
-    bot.on('message', function onMessage(msg) {
-  bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
-}); 
+  bot.on('callback_query', function onCallbackQuery(accionboton) {
+    const data = accionboton.data
+  
+    if (data == 'boton1'){
+      bot.answerCallbackQuery({
+        callback_query_id: accionboton.id,
+        text : 'Accion 1',
+        show_alert: true
+      })
+    }
+})})
+bot.onText(/^\/clima (.+)/, function(msg, match){
+    var chatId = msg.chat.id;
+    var ciudad = match[1];
+
+    var opciones = {
+        search: ciudad, 
+        degreeType: 'C', 
+        lang: 'es-ES' 
+    }
+
+    weather.find(opciones, function(err, result){
+
+        if (err){ 
+            console.log(err);
+
+        } else {
+            console.log(result[0]);
+            
+            bot.sendMessage(chatId, "Lugar: " + result[0].location.name +
+            "\n\nTemperatura: " + result[0].current.temperature + "ºC\n" +
+            "Visibilidad: " + result[0].current.skytext + "\n" +
+            "Humedad: " + result[0].current.humidity + "%\n" +
+            "Dirección del viento: " + result[0].current.winddisplay + "\n"
+            ,{parse_mode: 'Markdown'});
+
+        }
+    })
+});
